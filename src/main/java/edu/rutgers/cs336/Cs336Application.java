be2b90808382cb17f3cc88d53b20d75e85bbda8e
Jack Foltz <jack@foltz.io>
2021-11-03 17:55:45 -0400

    untabify

--- src/main/java/edu/rutgers/cs336/Cs336Application.java
+++ src/main/java/edu/rutgers/cs336/Cs336Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cs336Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Cs336Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Cs336Application.class, args);
+    }
 }
